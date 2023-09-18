c058c86d1e3ca0619adc525768f4e4d23201f2c1
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-09-18 11:02:59 +0300

    Update 01.river_info.sql

--- Built-in Functions - Exercise/01.river_info.sql
+++ Built-in Functions - Exercise/01.river_info.sql
@@ -1,1 +1,11 @@
-
+CREATE VIEW view_river_info AS
+SELECT 
+	CONCAT('The river', ' ',
+	"river_name", ' ',
+	'flows into the', ' ',
+	"outflow", ' ', 'and is',
+	' ', "length", ' ',
+	'kilometers long.')
+	AS "River Information"
+FROM rivers
+ORDER BY river_name;
