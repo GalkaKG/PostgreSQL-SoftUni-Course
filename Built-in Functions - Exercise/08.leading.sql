5ad3885bda1b3d9165282efa7af8ac2ef9bfe9af
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-09-20 19:59:09 +0300

    Create 08.leading.sql

---
+++ Built-in Functions - Exercise/08.leading.sql
@@ -0,0 +1,12 @@
+SELECT 
+	continent_name,
+	LTRIM(continent_name) AS "trim"
+FROM 
+	continents;
+
+
+-- SELECT 
+-- 	continent_name,
+-- 	TRIM(LEADING FROM continent_name) AS "trim"
+-- FROM 
+-- 	continents;
