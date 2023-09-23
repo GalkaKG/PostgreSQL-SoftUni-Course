616a1e2debdd9683100013b5fac17aa26c63fd83
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-09-23 14:35:25 +0300

    Update 10.wizard_view.sql

--- Data Aggregation - Exercise/10.wizard_view.sql
+++ Data Aggregation - Exercise/10.wizard_view.sql
@@ -8,12 +8,34 @@ SELECT
 	deposit_amount AS "Amount"
 FROM
 	wizard_deposits
+WHERE
+	deposit_expiration_date <= '1983-08-17'::date
 GROUP BY
 	"Wizard Name", 
 	"Start Date",
 	"Expiration Date",
 	"Amount"
-HAVING
-	deposit_expiration_date <= '1983-08-17'::date
 ORDER BY
 	"Expiration Date";
+
+
+
+-- CREATE OR REPLACE VIEW
+-- 	view_wizard_deposits_with_expiration_date_before_1983_08_17
+-- AS
+-- SELECT
+-- 	concat(first_name, ' ', last_name) AS "Wizard Name",
+-- 	deposit_start_date AS "Start Date",
+-- 	deposit_expiration_date AS "Expiration Date",
+-- 	deposit_amount AS "Amount"
+-- FROM
+-- 	wizard_deposits
+-- GROUP BY
+-- 	"Wizard Name", 
+-- 	"Start Date",
+-- 	"Expiration Date",
+-- 	"Amount"
+-- HAVING
+-- 	deposit_expiration_date <= '1983-08-17'::date
+-- ORDER BY
+-- 	"Expiration Date";
