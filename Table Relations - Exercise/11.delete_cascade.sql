a0022edd923c13c197337df9faa2994b4b3fbe3f
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-09-29 12:33:22 +0300

    Create 11.delete_cascade.sql

---
+++ Table Relations - Exercise/11.delete_cascade.sql
@@ -0,0 +1,10 @@
+-- CREATE DATABASE table_relations_geography_db;
+
+ALTER TABLE countries
+ADD CONSTRAINT fk_countries_continents
+FOREIGN KEY (continent_code) REFERENCES continents (continent_code)
+ON DELETE CASCADE,
+
+ADD CONSTRAINT fk_countries_currencies
+FOREIGN KEY (currency_code) REFERENCES currencies (currency_code)
+ON DELETE CASCADE;
