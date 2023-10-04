85bb680afe10f4c55e993794617343b34efc69ee
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-10-04 11:11:54 +0300

    Update 02.employees_promotion.sql

--- Database Programmability - Lab/02.employees_promotion.sql
+++ Database Programmability - Lab/02.employees_promotion.sql
@@ -7,16 +7,35 @@ $$
 		WHERE department_id =
 		(
 			SELECT
-				d.department_id
+				department_id
 			FROM
-				employees AS e
-			JOIN
-				departments AS d
-			USING
-				(department_id)
+				departments 
 			WHERE name = department_name
-			GROUP BY d.department_id
 		);
-	END
+	END;
 $$
 LANGUAGE plpgsql;
+
+
+-- CREATE OR REPLACE PROCEDURE sp_increase_salaries(department_name VARCHAR)
+-- AS
+-- $$
+-- 	BEGIN
+-- 		UPDATE employees
+-- 		SET salary = salary + salary * 0.05
+-- 		WHERE department_id =
+-- 		(
+-- 			SELECT
+-- 				d.department_id
+-- 			FROM
+-- 				employees AS e
+-- 			JOIN
+-- 				departments AS d
+-- 			USING
+-- 				(department_id)
+-- 			WHERE name = department_name
+-- 			GROUP BY d.department_id
+-- 		);
+-- 	END
+-- $$
+-- LANGUAGE plpgsql;
