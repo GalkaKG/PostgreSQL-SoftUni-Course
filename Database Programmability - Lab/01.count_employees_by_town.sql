682bdef7c2499dd1e469158d6b49fa0efa549e30
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-10-04 10:18:51 +0300

    Update 01.count_employees_by_town.sql

--- Database Programmability - Lab/01.count_employees_by_town.sql
+++ Database Programmability - Lab/01.count_employees_by_town.sql
@@ -2,23 +2,48 @@ CREATE OR REPLACE FUNCTION fn_count_employees_by_town(town_name VARCHAR(20))
 RETURNS INT AS
 $$
 	DECLARE
-	empl_count INT;
 	BEGIN
-		SELECT
-			count(*)
-		FROM
-			employees AS e
-		JOIN
-			addresses AS a
-		USING
-			(address_id)
-		JOIN 
-			towns AS t
-		USING
-			(town_id)
-		WHERE t.name = town_name
-		INTO empl_count;
-		RETURN empl_count;
+		RETURN(
+			SELECT
+				count(*)
+			FROM
+				employees AS e
+			JOIN
+				addresses AS a
+			USING
+				(address_id)
+			JOIN 
+				towns AS t
+			USING
+				(town_id)
+			WHERE t.name = town_name
+		);
 	END
 $$
 LANGUAGE plpgsql;
+
+
+-- CREATE OR REPLACE FUNCTION fn_count_employees_by_town(town_name VARCHAR(20))
+-- RETURNS INT AS
+-- $$
+-- 	DECLARE
+-- 	empl_count INT;
+-- 	BEGIN
+-- 		SELECT
+-- 			count(*)
+-- 		FROM
+-- 			employees AS e
+-- 		JOIN
+-- 			addresses AS a
+-- 		USING
+-- 			(address_id)
+-- 		JOIN 
+-- 			towns AS t
+-- 		USING
+-- 			(town_id)
+-- 		WHERE t.name = town_name
+-- 		INTO empl_count;
+-- 		RETURN empl_count;
+-- 	END
+-- $$
+-- LANGUAGE plpgsql;
