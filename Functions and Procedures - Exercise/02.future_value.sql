19359c35e8e8ddcc1e00d112a4a8c16ca5014825
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-10-05 18:32:34 +0300

    Create 02.future_value.sql

---
+++ Functions and Procedures - Exercise/02.future_value.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FUNCTION fn_calculate_future_value (
+	initial_sum DECIMAL,
+	yearly_interest_rate DECIMAL,
+	number_of_years INT
+) RETURNS DECIMAL AS 
+$$
+	BEGIN
+		
+	RETURN TRUNC(initial_sum * power(1 + yearly_interest_rate, number_of_years), 4);
+	END;
+$$
+LANGUAGE plpgsql;
